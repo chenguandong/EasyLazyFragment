e9bb959faa2ba5a4424837ab18fbc4ce44da4078
chenguandong <chenguandong@limijiaoyin.com>
2018-07-11 18:35:48 +0800

    init

---
+++ LazyFragment/LazyFragmentLib/src/main/java/com/cgd/lazyfragment/LazyFragment.java
@@ -0,0 +1,58 @@
+package com.cgd.lazyfragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+public abstract class LazyFragment extends Fragment {
+
+    // 标志位，标志已经初始化完成，因为setUserVisibleHint是在onCreateView之前调用的，
+    // 在视图未初始化的时候，在lazyLoad当中就使用的话，就会有空指针的异常
+    private boolean isPrepared;
+    //标志当前页面是否可见
+    private boolean isVisible;
+
+    public LazyFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        isPrepared = true;
+        //lazyLoad();
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        //懒加载
+        if (getUserVisibleHint()) {
+            isVisible = true;
+            onVisible();
+        } else {
+            isVisible = false;
+            onInvisible();
+        }
+    }
+
+    protected void onVisible() {
+        lazyLoad();
+    }
+
+    protected void lazyLoad() {
+        if (!isVisible || !isPrepared) {
+            return;
+        }
+        getData();//数据请求
+    }
+
+    protected void onInvisible() {
+
+    }
+
+    protected abstract void getData();
+
+
+
+}
---
+++ LazyFragment/app/src/main/java/com/yiwan/lazyfragment/MainActivity.java
@@ -0,0 +1,40 @@
+package com.yiwan.lazyfragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+
+    private List<Fragment>fragmentList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.viewPageer);
+
+        fragmentList = new ArrayList<>();
+        fragmentList.add(new EmptyFragment());
+        fragmentList.add(new EmptyFragment());
+        fragmentList.add(new EmptyFragment());
+        fragmentList.add(new DemoFragment());
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return fragmentList.get(i);
+            }
+
+            @Override
+            public int getCount() {
+                return fragmentList.size();
+            }
+        });
+    }
+}
